b1680172ebe62343d874c194b56c6458296de53f
James Houston <houstonj1@wit.edu>
2018-08-27 21:14:56 -0400

    Added connection and queue delaration

--- work_queues/new_task.py
+++ work_queues/new_task.py
@@ -1,2 +1,16 @@
+import pika
+import sys
+
+"""
+new_task.py
+
+Scheduler for tasks being sent to work queues
+"""
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
 
 
