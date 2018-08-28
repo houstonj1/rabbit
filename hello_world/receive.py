b8141f65c80bb56a46bc34652f323c849e290ccb
James Houston <houstonj1@wit.edu>
2018-08-27 21:00:10 -0400

    Added callback function to receive messages from 'hello' queue and logic to consume from the queue using this function

--- hello_world/receive.py
+++ hello_world/receive.py
@@ -6,7 +6,15 @@ receive.py
 Receive messages from queues
 """
 
+def callback(ch, method, properties, body):
+    """
+    Callback function - for receiving messages from Rabbit
+    """
+    print("  [x] Received {0}".format(body))
+
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
+
+channel.basic_consume(callback, queue='hello', no_ack=True)
