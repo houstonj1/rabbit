c885b28ad2d106eef2390b75061eaacb2ee33425
James Houston <houstonj1@wit.edu>
2018-08-27 20:47:34 -0400

    Created a 'hello' queue

--- hello_world/send.py
+++ hello_world/send.py
@@ -8,3 +8,5 @@ Send a single message to a queue
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
+
+channel.queue_declare(queue='hello')
