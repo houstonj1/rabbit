be2d97fe11ba843067fbcf9174eaf88c11d78732
James Houston <houstonj1@wit.edu>
2018-08-27 21:38:10 -0400

    Added callback function with task processing logic

--- work_queues/worker.py
+++ work_queues/worker.py
@@ -7,6 +7,14 @@ worker.py
 Worker to simulate a task given different sized messages
 """
 
+def callback(ch, method, properties, body):
+    """
+    Callback function - receives messages and performs a task
+    """
+    print("  [x] Received {0}".format(body))
+    time.sleep(body.count(b'.'))
+    print("  [x] Done")
+
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
