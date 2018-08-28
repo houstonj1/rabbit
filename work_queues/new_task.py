7f9232f1e7aa3db604592fb6e02d1504f3d3ad6a
James Houston <houstonj1@wit.edu>
2018-08-27 21:16:50 -0400

    Added ability to send messages from the CLI

--- work_queues/new_task.py
+++ work_queues/new_task.py
@@ -12,5 +12,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-
-
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(exchange='', routing_key='hello', body=message)
+print("  [x] Sent {0}".format(message))
