ffae2b6169a6a68cc518a8e10656cc8f90a81f16
James Houston <houstonj1@wit.edu>
2018-08-27 21:01:28 -0400

    Started loop to consume messages

--- hello_world/receive.py
+++ hello_world/receive.py
@@ -18,3 +18,5 @@ channel = connection.channel()
 channel.queue_declare(queue='hello')
 
 channel.basic_consume(callback, queue='hello', no_ack=True)
+print("  [*] Waiting for messages. To exit press CTRL-C")
+channel.start_consuming()
