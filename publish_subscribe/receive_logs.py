aa13ae33ebd4d09396e373758fb519806187a962
James Houston <houstonj1@wit.edu>
2018-08-29 20:41:54 -0400

    Declared a random queue that will be deleted after message is consumed (exclusive)

--- publish_subscribe/receive_logs.py
+++ publish_subscribe/receive_logs.py
@@ -10,3 +10,7 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
