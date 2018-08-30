e3231c9ef81aaf8a58293f7503e3316c1cb39bcc
James Houston <houstonj1@wit.edu>
2018-08-29 20:44:00 -0400

    Bound the exchange to the queue via name. Added startup log message

--- publish_subscribe/receive_logs.py
+++ publish_subscribe/receive_logs.py
@@ -14,3 +14,6 @@ channel.exchange_declare(exchange='logs', exchange_type='fanout')
 result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
 
+channel.queue_bind(exchange='logs', queue=result.method.queue)
+
+print("  [*] Waiting for logs. To exit press CTRL-C")
