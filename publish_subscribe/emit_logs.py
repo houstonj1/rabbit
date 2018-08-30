0d974e5908b8a30d0c6e374347d0ea7950ba76d7
James Houston <houstonj1@wit.edu>
2018-08-29 20:36:27 -0400

    Published message to the logs exchange and closed connection to rabbit

--- publish_subscribe/emit_logs.py
+++ publish_subscribe/emit_logs.py
@@ -11,3 +11,9 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+message = ' '.join(sys.argv[1:]) or 'info: Hello World'
+channel.basic_publish(exchange='logs', routing_key='', body=message)
+print("  [x] Sent {0}".format(message))
+
+connection.close()
